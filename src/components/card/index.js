e574314bdd931d51a9e1431c2a8a2336c085007a
Miguel Yax <miyax@paypal.com>
2021-02-10 18:22:28 -0600

    [main] flip cards

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './style.scss';
 
 const Card = ({ children }) => {
+const [flipped, setFlipped] = useState(false);
+
   return (
-    <div className="card" >
-      {children}
+  <div 
+    className="flip-card"
+    onClick={()=> {setFlipped(!flipped)}}
+  >
+    <div className={flipped ? "card-front card-front-rotate" : "card-front"}>
+      Front!
     </div>
+    <div className={flipped ? "card-back card-back-rotate" : "card-back"}>
+     {children}
+    </div>
+  </div>
   );
 }
 
